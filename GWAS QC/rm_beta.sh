136909bffcd2b30c600bf1448a12f894d21a966c
FedericaGrosso <148697309+fgrosso98@users.noreply.github.com>
2024-09-25 12:01:00 +0200

    Update rm_beta.sh

--- GWAS QC/rm_beta.sh
+++ GWAS QC/rm_beta.sh
@@ -14,7 +14,7 @@ fi
 for input_file in "$input_directory"/*
 do
   filename=$(basename "$input_file")
-  output_file="/home/students/federica.grosso/nas/microbiome/GWAS_new/$filename"
+  output_file="/home/.../nas/microbiome/GWAS_new/$filename"
   awk '{ if ($4 == "480000000" ) {$4 = "48000000";} print }' "$input_file" > "$output_file"
   Rscript RM_beta.R "$output_file"
 done
