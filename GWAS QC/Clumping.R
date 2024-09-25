bfdbaa904bdfa5cb12a00d4688988b41ccbcda35
FedericaGrosso <148697309+fgrosso98@users.noreply.github.com>
2024-09-25 11:57:26 +0200

    Update and rename CLUMPING_micr_server.R to Clumping.R

--- GWAS QC/Clumping.R
+++ GWAS QC/Clumping.R
@@ -1,14 +1,19 @@
+################################################################
+##### CLUMPING
+################################################################
 require(R.utils)
 require(TwoSampleMR)
 
-# Leggi il percorso del file dalla linea di comando
+# Read command line
 args <- commandArgs(trailingOnly = TRUE)
 file_path <- args[1]
+
+# Read exposure
 suppressWarnings({
   exp_data <- read_exposure_data(
     filename = file_path,
     phenotype_col = "trait",
-    sep = "\t",
+    sep = "\t", # or "" if not tabulated
     clump = F,
     snp_col = "rsid_to_use",
     beta_col = "beta",
@@ -30,16 +35,14 @@ cd <- clump_data(
   clump_r2 = 0.001,
   clump_p1 = 0.000005, #5e-6
   clump_p2 = 0.000005,
-  bfile = "/home/students/federica.grosso/nas/pop_reference/EUR_phase3", #riferimento
+  bfile = "/home/.../pop_reference/EUR_phase3", #ref
   plink_bin = "/home/shared_tools/bin/plink"
 )
 
 nome_fileE <- basename(file_path)
 nome_file_senza_ext <- tools::file_path_sans_ext(nome_fileE)
-# Divide il nome del file basandosi sul carattere "_"
 parti_nome <- strsplit(nome_file_senza_ext, "_")[[1]]
 access <- paste(parti_nome[2], sep = "_")
 
-#output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Clumping_results/clumping_", access, ".csv")
-output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Clumping_results_no_NA/clumping_", access, ".csv")
+output_file <- paste0("/home/.../microbiome/Clumping_results_no_NA/clumping_", access, ".csv")
 write.csv(cd, file = output_file, quote=F, row.names = F)
