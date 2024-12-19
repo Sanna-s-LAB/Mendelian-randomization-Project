bb9a567951830074964d5e0f89f94f63bfa194f2
FedericaGrosso <148697309+fgrosso98@users.noreply.github.com>
2024-12-19 09:48:00 +0100

    Update rm_beta.sh

--- GWAS QC/rm_beta.sh
+++ GWAS QC/rm_beta.sh
@@ -4,7 +4,7 @@
 ######## REMOVE -4<BETA<4 AND ADJUST BASE PAIR POSITION
 ####################################################################################
 
-input_directory="/home/.../microbiome/GWAS_microbiome"
+input_directory=".../GWAS_microbiome"
 if [ ! -d "$input_directory" ]; then
   echo "Directory $input_directory non trovata."
   exit 1
@@ -14,7 +14,7 @@ fi
 for input_file in "$input_directory"/*
 do
   filename=$(basename "$input_file")
-  output_file="/home/.../nas/microbiome/GWAS_new/$filename"
+  output_file=".../GWAS_new/$filename"
   awk '{ if ($4 == "480000000" ) {$4 = "48000000";} print }' "$input_file" > "$output_file"
   Rscript RM_beta.R "$output_file"
 done
