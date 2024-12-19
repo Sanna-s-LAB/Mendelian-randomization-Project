86a89950a5d7be921c2cf3a2a914cfcca3823b35
FedericaGrosso <148697309+fgrosso98@users.noreply.github.com>
2024-12-19 09:46:59 +0100

    Update RM_beta.R

--- GWAS QC/RM_beta.R
+++ GWAS QC/RM_beta.R
@@ -8,5 +8,5 @@ data <- read.table(path, header=TRUE, sep="", fill=T)
 data_filtered <- subset(data, beta > -4 & beta < 4)
 
 nome_file <- basename(path)
-output_file <- paste0("/home/.../microbiome/GWAS_new/new_", nome_file)
+output_file <- paste0(".../GWAS_new/new_", nome_file)
 write.table(data_filtered, file= output_file, sep="\t", row.names=FALSE, quote=F)
