bd69c3fe9d2e7b70bcc9cbb485229fd1c32f7a5d
FedericaGrosso <148697309+fgrosso98@users.noreply.github.com>
2024-12-19 09:48:22 +0100

    Update run_clumping.sh

--- GWAS QC/run_clumping.sh
+++ GWAS QC/run_clumping.sh
@@ -4,8 +4,8 @@
 #########################################################################
 Rscript -e "library(R.utils)"
 
-script_r="/home/.../microbiome/Clumping.R"
-folder="/home/.../microbiome/GWAS_final"
+script_r=".../Clumping.R"
+folder=".../GWAS_final"
 files=$(find "$folder" -type f -name "merged_*.txt")
 
 for file in $files
