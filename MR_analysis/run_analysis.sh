a31f715037825f73b83ec1bb906b0dc81e141d34
FedericaGrosso <148697309+fgrosso98@users.noreply.github.com>
2024-12-19 09:45:34 +0100

    Update run_analysis.sh

--- MR_analysis/run_analysis.sh
+++ MR_analysis/run_analysis.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 Rscript -e "library(R.utils)"
 
-R_script="/home/.../microbiome/Results_CAD/Prova_CAD.R"
-exposure_folder="/home/.../microbiome/Clumping_results_no_NA"
-file_path_outcome="/home/.../microbiome/Outcomes/Coronary artery diseases/CAD_META.gz"
+R_script=".../MR.R"
+exposure_folder=".../Clumping_results_no_NA"
+file_path_outcome=".../Outcome_file.gz"
 
 for exposure_file in $exposure_folder/*.csv; do
     Rscript "$R_script" "$exposure_file" "$file_path_outcome"
 done
 
 # FDR correction
-Rscript "/home/.../microbiome/Results_CAD/FDR_correction.R"
+Rscript ".../FDR_correction.R"
